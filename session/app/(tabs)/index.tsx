00ec961afd18d2ab3114d287364aa8cc50d18773
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2025-04-24 14:28:19 -0600

    small changes to root app _layout file

--- session/app/(tabs)/index.tsx
+++ session/app/(tabs)/index.tsx
@@ -1,5 +1,4 @@
 import { Text, View } from "react-native";
-
 export default function Index() {
   return (
     <View
@@ -9,7 +8,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{fontFamily: 'PlayfairDisplay' }}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
--- session/app/_layout.tsx
+++ session/app/_layout.tsx
@@ -1,5 +1,26 @@
 import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { useEffect } from "react";
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    PlayfairDisplay: require('../assets/fonts/PlayfairDisplay-VariableFont_wght.ttf'),
+    PlayfairDisplayItalic: require('../assets/fonts/PlayfairDisplay-Italic-VariableFont_wght.ttf'),
+  });
+
+  useEffect (() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Stack/>
+  );
 }
