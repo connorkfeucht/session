f2bb0f909dc6cb1bc055f696193173983173b6d8
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2025-05-11 10:45:59 -0700

    profiles.tsx fetches user information from profiles table in DB

--- session/app/(tabs)/3-profile.tsx
+++ session/app/(tabs)/3-profile.tsx
@@ -3,18 +3,21 @@ import {
   View,
   Image,
   FlatList,
-  TouchableOpacity, 
+  TouchableOpacity,
+  ActivityIndicator, 
 } from "react-native";
 import { useRouter } from "expo-router";
+import { useState, useEffect } from "react";
 import { supabase } from "../../lib/supabase";
 import styles from "../styles";
 
-// DUMMY USER INFO
-// TODO: FETCH REAL USER INFO AND STORE IN STATE
-const USER = {
-  username: "johndoe",
-  bio: "crushing pomodoros since 2003",
-  seshnsCompleted: 50,
+type ProfileRow = {
+  id: string,
+  username: string, // TODO: in supabase this is currently nullable, need to change
+  full_name: string | null,
+  avatar_url: string | null;
+  bio: string | null;
+  seshns_completed: number;
 }
 
 // DUMMY DATA UNTIL HOOKED UP TO DB
@@ -27,6 +30,38 @@ const ACTIVITIES = Array.from({ length: 6 }).map((_, i) => ({
 
 export default function Profile() {
   const router = useRouter();
+  const [profile, setProfile] = useState<ProfileRow | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadProfile = async () => {
+      try {
+        // get the session to find the user id
+        const {
+          data: { session },
+        } = await supabase.auth.getSession();
+        const userId = session?.user.id;
+        if (!userId) throw new Error("No user session");
+
+        // query profiles table for that id
+        const { data, error } = await supabase
+          .from("profiles")
+          .select("id, username, full_name, avatar_url, bio, seshns_completed")
+          .eq("id", userId)
+          .single();
+
+        if (error) throw error;
+        setProfile(data);
+
+      } catch(error: any) {
+        console.error("Error loading profile:", error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadProfile();
+  }, []);
 
   const handleSignOut = async () => {
     const { error } = await supabase.auth.signOut();
@@ -38,14 +73,30 @@ export default function Profile() {
     }
   }
 
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large"/>
+      </View>
+    );
+  }
+
+  if (!profile) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.statsText}>Profile Not Found.</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profileHeader}>
-        <Image source={{uri: "https://avatars.githubusercontent.com/u/132318582?v=4"}} style={styles.avatar}/>
+        <Image source={{uri: profile.avatar_url || "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg?20200418092106"}} style={styles.avatar}/>
         <View style={styles.profileInfo}>
-          <Text style={styles.username}>@{USER.username}</Text>
-          <Text style={styles.bio}>{USER.bio}</Text>
-          <Text style={styles.statsText}>{USER.seshnsCompleted} Completed Seshns</Text>
+          <Text style={styles.username}>@{profile.username.toLowerCase()}</Text>
+          <Text style={styles.bio}>{profile.bio}</Text>
+          <Text style={styles.statsText}>{profile.seshns_completed} Completed Seshns</Text>
         </View>
       </View>
     
